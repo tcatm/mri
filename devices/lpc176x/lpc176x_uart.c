e5336377cdcda3a29bfad6c8c3d82071e53bcd15
Nils Schneider <nils.schneider@posteo.de>
2024-09-28 21:05:16 +0200

    monkeypatch for UART2 on Carvera

--- devices/lpc176x/lpc176x_uart.c
+++ devices/lpc176x/lpc176x_uart.c
@@ -47,13 +47,13 @@ static const UartConfiguration g_uartConfigurations[] =
     },
     {
         &(LPC_SC->PCLKSEL1),
-        &(LPC_PINCON->PINSEL0),
-        &(LPC_PINCON->PINSEL0),
+        &(LPC_PINCON->PINSEL4),
+        &(LPC_PINCON->PINSEL4),
         (LPC_UART_TypeDef*)LPC_UART2,
         1 << 24,
         3 << 16,
-        3 << 20,
-        3 << 22,
+        2 << 16,
+        2 << 18,
         0x55555555
     },
     {
